b11224e851a56bafff98a707ec99500b963efcab
Millie Green <milliegreen@msdits-MacBook-Pro.local>
2021-09-06 14:00:47 +0100

    use correct term and lowercase

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -63,7 +63,7 @@ study = StudyDefinition(
     ADprecovid = patients.with_these_medications(
             antidepressantsall_codes,
             between = ["2019-03-01", "2020-03-22"],
-            return_last_date_in_period=TRUE, # to obtain latest date on AD preCOVID
+            find_last_match_in_period = True, # to obtain latest date on AD preCOVID
             return_expectations={"earliest": "2019-03-01", "latest": "2020-03-22"},
             returning = "binary_flag"
             
@@ -72,7 +72,7 @@ study = StudyDefinition(
     ADpostcovid = patients.with_these_medications(
             antidepressantsall_codes,
             between = ["2020-03-23", "2021-03-31"],
-            return_last_date_in_period=TRUE, # to obtain latest date on AD preCOVID
+            find_last_match_in_period = True, # to obtain latest date on AD preCOVID
             return_expectations={"earliest": "2020-03-23", "latest": "2021-03-31"},
             returning = "binary_flag"
 
