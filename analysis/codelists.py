2fc1737afe946ac015dea754ae229ffcbc685669
Millie Green <acagreen17@gmail.com>
2021-09-06 14:59:55 +0100

    use nisha's codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -16,15 +16,15 @@ from cohortextractor import (codelist, codelist_from_csv, combine_codelists)
 # --CODELISTS--
 
 # Antidepressants - All
-#antidepressantsall_codes = codelist_from_csv(
-#  "codelists/user-nish0119-antidepressant-drugs-all-bnf-chapter-4-section-3.csv",
-#  system = "snomed",
-#  column = "dmd_id",
-#)
-
-# Antidepressants - SSRI
 antidepressantsall_codes = codelist_from_csv(
-  "codelists/opensafely-selective-serotonin-reuptake-inhibitors-dmd-6a572e55.csv",
-  system = "snomed",
-  column = "dmd_id",
+ "codelists/user-nish0119-antidepressant-drugs-all-bnf-chapter-4-section-3.csv",
+ system = "snomed",
+ column = "code",
 )
+
+# # Antidepressants - SSRI
+# antidepressantsall_codes = codelist_from_csv(
+#   "codelists/opensafely-selective-serotonin-reuptake-inhibitors-dmd-6a572e55.csv",
+#   system = "snomed",
+#   column = "dmd_id",
+# )
