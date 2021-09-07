b7414e49828d0ccf16da87f76d425fdbdb489bd6
nisha0119 <nish.rajendran@gmail.com>
2021-09-07 11:47:34 +0000

    Region variable added

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -91,5 +91,24 @@ study = StudyDefinition(
     },
     ),
 
+    region = patients.registered_practice_as_of (
+        "2020-02-01",
+        returning = "nuts1_region_name",
+        return_expectations = {
+            "rate": "universal",
+            "category": {
+            "ratios": {
+                "North East": 0.1,
+                "North West": 0.1,
+                "Yorkshire and the Humber": 0.1,
+                "East Midlands": 0.1,
+                "West Midlands": 0.1,
+                "East of England": 0.1,
+                "London": 0.2,
+                "South East": 0.2,
+            },
+        },
+    },
+    ),
 
 )
