503e3d3f3964446039ffac45921bdc7d6c1827dd
nisha0119 <nish.rajendran@gmail.com>
2021-10-05 12:29:32 +0000

    Updating codelistspy with ethnicity

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -28,3 +28,10 @@ antidepressantsall_codes = codelist_from_csv(
   system = "snomed",
   column = "dmd_id",
 )
+
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
